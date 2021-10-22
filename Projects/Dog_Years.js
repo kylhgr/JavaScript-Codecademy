c1eb5178461eb805b91fa068f689fe89a61933ef
Kaylah Gore <43583709+kylhgr@users.noreply.github.com>
2021-10-22 01:29:59 +0000

    adding dog years project

---
+++ Projects/Dog_Years.js
@@ -0,0 +1,20 @@
+let myAge = 26;
+console.log(myAge);
+
+let earlyYears = 2;
+earlyYears = earlyYears *  10.5;
+console.log(earlyYears);
+
+let laterYears = myAge - 2;
+console.log(laterYears);
+laterYears = laterYears * 4;
+console.log(laterYears);
+
+let myAgeInDogYears = (laterYears * earlyYears);
+console.log(myAgeInDogYears);
+
+var myName = 'Kaylah Gore'.toLowerCase();
+console.log(myName);
+
+const greeting = `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+console.log(greeting)
