57ef209cbba4b00afcbe103c55c91e8bbcc3c39b
Kaylah Gore <43583709+kylhgr@users.noreply.github.com>
2021-10-22 01:39:42 +0000

    uploading conditional statements lesson

---
+++ Lessons/conditional-statement.js
@@ -0,0 +1,9 @@
+
+let sale = true
+
+if (sale) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale')
+}
+
---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('Hello World');
+//output Hello World
+
+let greeting = 'Hello, World';
+console.log(greeting);
+
+typeof(greeting);
